7877fdead8ac52e7c7ff5ee7bc0d141c59714900
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-27 00:36:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,14 @@ end
 planeteer_calls
 end 
 
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array = string.split (" ")
+  if string.length > 5 
+    return "true"
+  else 
+    string.length <5 
+    return false 
+  end 
 end
 
 def find_the_cheese# code an argument here
